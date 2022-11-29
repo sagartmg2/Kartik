d4c5a98c49518a1a285be19914e054fa5e1e050c
Sagar <dev.sagartmg@gmail.com>
2022-11-29 16:57:41 +0545

    js varaible and tasks

---
+++ tasks/2.6-swap.js
@@ -0,0 +1,12 @@
+let a = 1;
+let b = 2;
+
+
+console.log(a, b);
+
+/* 
+    the values of variable should be interchanged.
+    output a: 2 
+    output b: 1 
+
+*/
---
+++ tasks/2.7.js
@@ -0,0 +1,12 @@
+
+console.log(" double quote = ""  ")
+
+/* 
+
+    output: double quote = ""
+
+*/
+/* 
+    can use template literal or escape character
+*/
+
