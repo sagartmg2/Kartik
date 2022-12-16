67726c518be7827f19300c214467d2a477f44c28
Sagar <dev.sagartmg@gmail.com>
2022-12-16 16:58:43 +0545

    todos

---
+++ tasks/14 todo list/araryToStringConversion.js
@@ -0,0 +1,27 @@
+let arr = ["one", "two"]
+let expected_str = " <li>one</li><li>tow</li>"
+
+
+let str = "";
+str = ""
+
+
+// for (let i = 0; i < arr.length; i++) {
+//     str = str + `<li>${arr[i]}</li>`
+// }
+
+// arr.forEach((element) => {
+//     str = str + `<li>${element}</li>`
+// })
+
+// let new_mapped_data = arr.map((element) => {
+//     return `<li>${element}</li>`
+// })
+
+console.log(new_mapped_data);
+
+
+console.log(str);
+
+
+
